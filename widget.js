6f627e8eec63b4d3729d82bc676f9235d5f603cc
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-06-21 17:59:45 +0200

    performance test @lighthouse

--- widget.js
+++ widget.js
@@ -3,44 +3,48 @@ const addCSS = () => {
   const styleElement = document.createElement("style");
   styleElement.textContent = `
   .campspace-widget {
-    --color-campspace: #0db08a;
-    --color-yellow-campspace: #fdb000;
-    --color-light-grey-campspace: #f5f5f1;
-    --color-dark-grey-campspace: #32322e;
-    --color-blue-campspace: #4376ba;
-    --color-primary-campspace: var(--color-campspace);
-    --color-secondary-campspace: var(--color-yellow);
-    --color-tertiary-campspace: var(--color-blue);
-    --border-radius-small-campspace: 5px;
-    --border-radius-medium-campspace: 8px;
-    --border-radius-large-campspace: 16px;
-    box-sizing: border-box;
-    margin: 0;
-    font-family: Arial, Helvetica, sans-serif;
+      --color-campspace: #0db08a;
+      --color-yellow-campspace: #fdb000;
+      --color-light-grey-campspace: #f5f5f1;
+      --color-dark-grey-campspace: #32322e;
+      --color-blue-campspace: #4376ba;
+      --color-darkBlue-campspace: #294060;
+      --color-primary-campspace: var(--color-campspace);
+      --color-secondary-campspace: var(--color-yellow);
+      --color-tertiary-campspace: var(--color-blue);
+      --border-radius-small-campspace: 5px;
+      --border-radius-medium-campspace: 8px;
+      --border-radius-large-campspace: 16px;
+      box-sizing: border-box;
+      margin: 0;
+      font-family: Arial, Helvetica, sans-serif;
   }
   
   .campspace-button {
     position: fixed;
     right: 20px;
     top: 50%;
-    padding: 15px 30px;
+    padding: 20px 50px;
     background-color: var(--color-campspace);
     color: var(--color-light-grey-campspace);
     border-radius: 8px 8px 0 0;
     border: none;
     cursor: pointer;
     z-index: 9999;
+    font-size: 20px;
     transform: translate(50%, -50%) rotate(-90deg);
   }
+
   
   .campspace-widget article {
+    border-radius: var(--border-radius-medium-campspace);
     border:none;
-    width: clamp(15em, 50%, 34em);
+    width: clamp(15em, 50%, 27em);
     padding: 0;
     overflow: hidden;
 }
 
-.widget-images-slider picture{
+.widget-images-slider  {
       border-radius: var(--border-radius-medium-campspace);
     display: flex;
     width: 100%;
@@ -88,6 +92,13 @@ const addCSS = () => {
     touch-action: manipulation;
     padding: 1rem 2rem;
   }
+
+  .widget-content-overlay a:hover {
+    background-color:  var(--color-darkBlue-campspace)
+
+  }
+
+
   
   @keyframes slide {
     0%, 33% {
@@ -100,45 +111,43 @@ const addCSS = () => {
       transform: translateX(-200%);
     }
   }
-
   }
 
 
     `;
+
   document.head.appendChild(styleElement);
 };
-
-console.log("test");
 
 // Function to create and append the widget to the body
 const createWidget = (data) => {
   const section = document.createElement("section");
   section.className = "campspace-widget";
-  // I want to load a image from the json file
+
+  // Find the host with id 1
+  const host = data.find((host) => host.id === "1");
+
+  // Fetch the second image from the images array of the host
+  const firstImage = host.images[0];
+  const secondImage = host.images[1];
+  const thirdImage = host.images[2];
+
   const widgetContent = `
-        <button class="campspace-button" popovertarget="my-popover-widget" popovertargetaction="show">
-          Book Now
-        </button>
-        <article popover id="my-popover-widget">
-        
-         <div class="widget-images-slider">
-          <picture>
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/webp">
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/jpeg">
-  <img src="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" alt="">
+  <button class="campspace-button" popovertarget="my-popover-widget" popovertargetaction="show">
+    Book Now
+  </button>
+  <article popover id="my-popover-widget">
+    <div class="widget-images-slider">
+        <img width="350" height="350" src="${firstImage.image}" alt="">
+      
 
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/webp">
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/jpeg">
-  <img src="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" alt="">
-
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/webp">
-  <source srcset="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" type="image/jpeg">
-  <img src="https://campspace.com/media/detail/uploads/space/hc/_1/ab/hc_1ab9feda225824f00656866e916cfd6d.jpeg" alt="">
-  </picture>
-         </div>
-          
+        <img width="350" height="350" src="${secondImage.image}" alt="">
+     
+      
+        <img width="350" height="350" src="${thirdImage.image}" alt="">
+    </div>
           <div class="widget-content-overlay">
-          <img src="${window.location.href}${data[0].campspace_logo}" alt="logo campspace">
+          <img src="${data[0].campspace_logo}" alt="logo campspace">
           <p> From €${data[0].price}</p>
             <div>
               <a href="${data[0].booking_url}">
@@ -147,7 +156,6 @@ const createWidget = (data) => {
             </div>
           </div>
         </article>
-
     `;
 
   section.innerHTML = widgetContent;
@@ -157,13 +165,13 @@ const createWidget = (data) => {
 };
 
 // Fetch the data from the API
-fetch("https://campspace-dummy-data-bb26a5c22467.herokuapp.com/hosts")
+fetch("https://campspace-dummy-data-test-a454ba90df28.herokuapp.com/hosts")
   .then((response) => {
     if (response.ok) {
       return response.json();
     }
   })
   .then((data) => {
-    console.log("gelukt", data);
+    console.log("succses", data);
     createWidget(data);
   });
