4f6eb42524b33eeba7916b1ea37b69e15d52d1f2
Michelle Hendriks <106346778+mcphendriks@users.noreply.github.com>
2023-06-25 15:01:13 +0200

    Refactor classnames

--- widget.js
+++ widget.js
@@ -1,4 +1,3 @@
-// Function to create and append the widget to the body
 const createWidget = async () => {
   try {
     const response = await fetch(
@@ -8,35 +7,34 @@ const createWidget = async () => {
     const section = document.createElement("section");
     section.classList.add("campspace-widget");
 
-    // Find the host with id 1 and fetch the images from the host
     const host = data.find((host) => host.id === "1");
     const [firstImage, secondImage, thirdImage] = host.images;
 
     const widgetContent = `
       <style>
+
       .campspace-widget {
-        --color-campspace: #0db08a;
-        --color-yellow-campspace: #fdb000;
-        --color-light-grey-campspace: #f5f5f1;
-        --color-dark-grey-campspace: #32322e;
-        --color-blue-campspace: #4376ba;
-        --color-darkBlue-campspace: #294060;
-        --border-radius-small-campspace: 5px;
-        --border-radius-medium-campspace: 8px;
-        --border-radius-large-campspace: 16px;
+        --color-green: #0db08a;
+        --color-light-grey: #f5f5f1;
+        --color-dark-grey-: #32322e;
+        --color-blue-: #4376ba;
+        --color-dark-blue: #294060;
+        --border-radius-small: 5px;
+        --border-radius-medium: 8px;
+       --border-radius-large: 16px;
         box-sizing: border-box;
         margin: 0;
         font-family: Arial, Helvetica, sans-serif;
       }
-      
+
       .campspace-button {
         font-weight: 600;
         position: fixed;
         right: 20px;
         top: 50%;
         padding: 20px 50px;
-        background-color: var(--color-campspace);
-        color: var(--color-light-grey-campspace);
+        background-color: var(--color-green);
+        color: var(--color-light-grey);
         border-radius: 8px 8px 0 0;
         border: none;
         cursor: pointer;
@@ -44,68 +42,68 @@ const createWidget = async () => {
         font-size: 20px;
         transform: translate(50%, -50%) rotate(-90deg);
       }
-      
+
       .campspace-widget article {
-        border-radius: var(--border-radius-medium-campspace);
+        border-radius: var(--border-radius-medium);
         border: none;
         width: clamp(15em, 50%, 27em);
         padding: 0;
         overflow: hidden;
       }
-      
-      .widget-images-slider {
-        border-radius: var(--border-radius-medium-campspace);
+
+      .campspace-widget-images-slider {
+        border-radius: var(--border-radius-medium);
         display: flex;
         width: 100%;
         height: 100%;
         animation: slide 16s infinite;
         list-style-type: none;
       }
-      
-      .widget-images-slider img {
+
+      .campspace-widget-images-slider img {
         width: 100%;
       }
-      
-      .widget-content-overlay div {
+
+      .campspace-widget-content-overlay div {
         display: flex;
         justify-content: center;
         bottom: 0;
       }
-      
-      .widget-content-overlay img {
+
+      .campspace-widget-content-overlay img {
         position: absolute;
         width: 10em;
         left: 10px;
         top: 10px;
       }
-      
-      .widget-content-overlay p {
+
+      .campspace-widget-content-overlay p {
         position: absolute;
         right: 10px;
         top: -7px;
         font-weight: 600;
         padding: 0.5rem;
-        border-radius: var(--border-radius-medium-campspace);
-        color: var(--color-light-grey-campspace);
-        background-color: var(--color-dark-grey-campspace);
+        border-radius: var(--border-radius-medium);
+        color: var(--color-light-grey);
+        background-color: var(--color-dark-grey-);
       }
-      
-      .widget-content-overlay a {
+
+      .campspace-widget-content-overlay a {
         position: absolute;
         bottom: 50px;
         font-weight: 600;
-        color: var(--color-light-grey-campspace);
-        background-color: var(--color-blue-campspace);
+        color: var(--color-light-grey);
+        background-color: var(--color-blue-);
         border-radius: 8px;
         text-decoration: none;
         touch-action: manipulation;
         padding: 1rem 2rem;
       }
-      
-      .widget-content-overlay a:hover {
-        background-color: var(--color-darkBlue-campspace);
+
+      .campspace-widget-content-overlay a:hover {
+        background-color: var(--color-dark-blue);
       }
-      
+
       @keyframes slide {
         0%, 33% {
           transform: translateX(0);
@@ -117,18 +115,18 @@ const createWidget = async () => {
           transform: translateX(-200%);
         }
       }
-      
+   
       </style>
       <button class="campspace-button" popovertarget="my-popover-widget" popovertargetaction="show">
         Book Now
       </button>
       <article popover id="my-popover-widget">
-        <div class="widget-images-slider">
-          <img src="${firstImage.image}"  width="450" height="" alt="">
-          <img src="${secondImage.image}"  width="450" height="450" alt="">
-          <img src="${thirdImage.image}"  width="450" height="450" alt="">
+        <div class="campspace-widget-images-slider">
+          <img src="${firstImage.image}" width="450" height="" alt="">
+          <img src="${secondImage.image}" width="450" height="450" alt="">
+          <img src="${thirdImage.image}" width="450" height="450" alt="">
         </div>
-        <div class="widget-content-overlay">
+        <div class="campspace-widget-content-overlay">
           <img src="${data[0].campspace_logo}" alt="logo campspace">
           <p>From €${data[0].price}</p>
           <div>
@@ -143,6 +141,8 @@ const createWidget = async () => {
     section.innerHTML = widgetContent;
     document.body.appendChild(section);
   } catch (error) {
-    console.error("An error occurred:", error);
+    console.error(error);
   }
 };
+
+createWidget();
